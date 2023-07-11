3edd5db8917db8faace2a9228f7d21e44b11f737
mxrgxr <mgxrcias@gmail.com>
2023-07-11 12:07:15 -0700

    implement OAuth login/logout functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wanderlust' });
 });
 
 module.exports = router;
