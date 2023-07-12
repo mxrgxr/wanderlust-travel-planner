5d782c5a01a28e2089b0ed4c448ca04ba3ca1c13
mxrgxr <mgxrcias@gmail.com>
2023-07-11 22:21:28 -0700

    add create itinerary function

--- controllers/itineraries.js
+++ controllers/itineraries.js
@@ -2,9 +2,29 @@ const Itinerary = require('../models/itinerary');
 
 module.exports = {
     index,
+    new: newItinerary,
+    create
 }
 
 async function index(req, res) {
     const itineraries = await Itinerary.find({user: req.user._id});
     res.render('itineraries/index', {itineraries, user: req.user, title: 'User Itineraries'});
+}
+
+function newItinerary(req, res) {
+    res.render('itineraries/new', { title: 'Create New Itinerary' });
+  }
+
+async function create(req, res) {
+const itineraryData = {
+    name: req.body.name,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    user: req.user._id,
+};
+
+const newItinerary = new Itinerary(itineraryData);
+await newItinerary.save();
+
+res.redirect('/itineraries');
 }
--- routes/itineraries.js
+++ routes/itineraries.js
@@ -4,5 +4,7 @@ const itinerariesCtrl = require('../controllers/itineraries');
 const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 router.get('/', ensureLoggedIn, itinerariesCtrl.index);
+router.get('/new', ensureLoggedIn, itinerariesCtrl.new);
+router.post('/new', ensureLoggedIn, itinerariesCtrl.create);
 
 module.exports = router;
