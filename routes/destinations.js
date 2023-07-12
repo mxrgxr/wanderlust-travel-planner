b0683dc0f09aa6e9a5855e2a9070784b790bfdf4
mxrgxr <mgxrcias@gmail.com>
2023-07-12 14:00:30 -0700

    render multiple destinations and allow user to keep adding destinations to itinerary

--- routes/destinations.js
+++ routes/destinations.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const destinationsCtrl = require('../controllers/destinations');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-router.get('/itinerary/:id/destinations', destinationsCtrl.new);
-router.post('/itinerary/:id/destination', destinationsCtrl.create);
+router.get('/itinerary/:id/destinations', ensureLoggedIn, destinationsCtrl.new);
+router.post('/itinerary/:id/destination', ensureLoggedIn, destinationsCtrl.create);
 
 module.exports = router;
